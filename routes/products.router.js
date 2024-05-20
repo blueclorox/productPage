f455a466d1520766d92a40bea610044651fcfd52
blueclorox <dehydrate92@gmail.com>
2024-05-20 15:19:33 +0900

    파일명 변경

--- routes/products.router.js
+++ routes/products.router.js
@@ -25,8 +25,27 @@ router.get('/products/:id', async (req,res) => {
     return res.status(200).json({status:200, message: '상품 상세 조회 성공', data})
 })
 
-router.put('/products/:id', (req,res) => {
+router.put('/products/:id', async (req,res) => {
+    const { id } = req.params
+    const {productName, productDescription, productManager, productPassword} = req.body
 
+    const existedProduct = await Product.findById(id, { productPassword: true})
+    const isPasswordMatched = productPassword === existedProduct.productPassword;
+    if(!isPasswordMatched){
+        return res.status(401).json({
+            status: 401,
+            message: '비밀번호가 틀렸습니다'
+        })
+    }
+
+    const productInfo = {
+        ...(productName && {productName}),
+        ...(productDescription && {productDescription}),
+        ...(productManager && {productManager}),
+    }
+    
+    const data = await Product.findByIdAndUpdate(id, productInfo);
+    return res.status(200).json({status:200, message: '상품 수정 성공', data})
 })
 
 router.delete('/products/:id', (req,res) => {
